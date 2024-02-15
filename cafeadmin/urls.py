0dcf8c355a9cd57d1729314b02e2055a320dd7a4
Thomas Meder <thomas.meder@sap.com>
2024-02-15 12:28:44 +0100

    redirect root to planner

--- cafeadmin/urls.py
+++ cafeadmin/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='planner')),
+    path("/", RedirectView.as_view(url='planner')),
     path("admin/", admin.site.urls),
     path("planner/", include("shift_planner.urls")),
     path("accounts/", include('django.contrib.auth.urls')),
-    
 ]
